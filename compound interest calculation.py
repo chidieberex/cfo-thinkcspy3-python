d4f61fc6aca80d096417396c5719d4111e8f4a6c
chidieberex <chidieberex@users.noreply.github.com>
2020-11-09 16:28:03 -0600

    Update compound interest calculation.py
    Included USD and did a few grammar corrections/modifications.

--- compound interest calculation.py
+++ compound interest calculation.py
@@ -3,5 +3,5 @@ n = 12
 r = 8/100
 t = float(input("How long do you want to compound your principal?: "))
 amount = principal * ((1 + (r / n)) ** (n * t))
-print("For an investment of " + str(principal) + " over " + str(n) + " times of interest compounding per year at a rate of " +
-      str(r) + " the final amount after " + str(int(t)) + " years" + " will be " + str(amount) + ".")
+print("For an investment of " + "USD" + str(principal) + " with up to " + str(n) + " times of interest compounding per year at a rate of " +
+      str(r) + " the final amount after " + str(int(t)) + " years of investment" + " will be " + "USD" + str(round(amount)) + ".")
