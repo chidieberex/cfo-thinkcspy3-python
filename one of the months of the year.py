aa465c70da8b06f18f5ee5870cbb6f40f90f8cc1
chidieberex <chidieberex@users.noreply.github.com>
2020-11-13 10:42:29 -0600

    one of the months of the year
    #also having difficulty saving this file. just giving it another try.

---
+++ one of the months of the year.py
@@ -0,0 +1,7 @@
+# 3.8 Exercises, number 3:
+
+months = ["January", "February", "March", "April", "May", "June",
+          "July", "August", "September", "October", "November", "December"]
+for c in months:
+    sentence = "One of the months of the year is " + c
+    print(sentence)
