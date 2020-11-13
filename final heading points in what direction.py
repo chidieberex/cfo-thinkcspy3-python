0b40a6de5ce916a6690f9965dda78de1a0fb5103
chidieberex <chidieberex@users.noreply.github.com>
2020-11-13 10:40:30 -0600

    final heading points in what direction
    #also having difficulty saving this file. just giving it another try.

---
+++ final heading points in what direction.py
@@ -0,0 +1,13 @@
+# 3.8 Exercises, number 4:
+
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("gray")
+wn.title("tess exercise")
+
+tess = turtle.Turtle()
+
+tess.left(3645)
+
+wn.mainloop()
+# tess goes in circles and her final heading points in the NE direction
