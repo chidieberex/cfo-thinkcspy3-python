18cec62059718c4e26b0028975a629c63fb79726
chidieberex <chidieberex@users.noreply.github.com>
2020-11-13 10:39:46 -0600

    cellphone attributes and methods
    #also having difficulty saving this file. just giving it another try.

---
+++ cellphone attributes and methods.py
@@ -0,0 +1,5 @@
+# 3.8 Exercises, number 2:
+# cellphone attributes: color, size, shape
+# cellphone methods: cellphone.power(on/off), cellphone.charging(yes/no), cellphone.ringing(mute/ring)
+
+#also having difficulty saving this file. just giving it another try.
