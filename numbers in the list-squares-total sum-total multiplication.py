b2d55e0a167cc4d01ace0a2738b556ec129a9fdf
chidieberex <chidieberex@users.noreply.github.com>
2020-11-13 10:42:11 -0600

    numbers in the list-squares-total sum-total multiplication
    #also having difficulty saving this file. just giving it another try.

---
+++ numbers in the list-squares-total sum-total multiplication.py
@@ -0,0 +1,43 @@
+'''
+# 3.8 Exercises, number 5a:
+xs = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+
+for c in xs:
+    ca = str(c) + " is one of the numbers in the list."
+    print(ca)
+'''
+
+'''
+
+# 3.8 Exercises; number 5b:
+
+xs = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+
+for c in xs:
+    ca = str(
+        c) + " is one of the numbers in the list and its square is " + str(c * c) + "."
+    print(ca)
+
+'''
+
+'''
+# 3.8 Exercises, number 5c:
+
+total = 0
+xs = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+for c in xs:
+    total += c
+    # print(total)
+print(total)
+'''
+
+'''
+# 3.8 Exercises, number 5d:
+
+total = 1
+xs = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+for c in xs:
+    total *= c
+    print(total)
+print(total)
+'''
